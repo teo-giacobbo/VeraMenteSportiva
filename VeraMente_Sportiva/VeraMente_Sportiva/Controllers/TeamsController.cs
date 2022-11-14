517a82f5b7897397cfda04234506255bdd8764b0
Teo Giacobbo <teo.giacobbo@it-manager.it>
2022-11-14 15:26:44 +0100

    Inizio gestione squadra

---
+++ VeraMente_Sportiva/VeraMente_Sportiva/Controllers/TeamsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VeraMente_Sportiva.Controllers
+{
+    public class TeamsController : Controller
+    {
+        public ActionResult Index(string championship)
+        {
+            ViewBag.Championship = championship;
+            return View();
+        }
+
+        public ActionResult Team(string championship)
+        {
+            return PartialView();
+        }
+    }
+}
